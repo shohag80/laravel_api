f0d18469064a73aeb0e60df83acf608694774301
Md. Shohag Hossain <shohag@atilimited.net>
2025-04-17 16:31:32 +0600

    test feature

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    // use RefreshDatabase; // if need to database fresh
+    /**
+     * A basic feature test example of register.
+     */
+    // public function test_register(): void
+    // {
+    //     $credentials = [
+    //         'name' => 'mdshohag',
+    //         'email' => 'mdshohag9090@gmail.com',
+    //         'password' => '12345678',
+    //         'password_confirmation' => '12345678'
+    //     ];
+
+    //     $response = $this->postJson('api/register', $credentials);
+
+    //     $response->assertStatus(200);
+    // }
+
+    /**
+     * A basic feature test example of test login.
+     */
+    // public function test_login(): void
+    // {
+    //     $credentials = [
+    //         'email' => 'mdshohag9090@gmail.com',
+    //         'password' => '12345678',
+    //     ];
+
+    //     $response = $this->postJson('api/login', $credentials);
+
+    //     $response->assertStatus(200);
+    // }
+}
